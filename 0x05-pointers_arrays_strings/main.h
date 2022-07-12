42e594b5fef1a983a9e481d46e0e4a241e4bf256
birhanuhirpasa <birhanuhirpasam@gmail.com>
2022-07-12 12:58:15 -0700

    c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 void print_rev(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -6,7 +8,8 @@ void _puts(char *str);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
 
+#endif
