454dec3eba5917dd10578fc7a28a56a666c80108
birhanuhirpasa <birhanuhirpasam@gmail.com>
2022-07-12 12:44:53 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,1 +1,12 @@
 void print_rev(char *s);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);char *_strcpy(char *dest, char *src);
+
